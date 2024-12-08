cdf4338df6f10e92b42805846a0a940ff89e133e
Pat Needham <pat@needham.io>
2024-12-08 18:11:17 -0500

    wrangler file additions for CloudFlare

--- apps/landing-page/app/nx-welcome.tsx
+++ apps/landing-page/app/nx-welcome.tsx
@@ -432,7 +432,7 @@ export function NxWelcome({ title }: { title: string }) {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome {title} 👋
+              Welcome to Polyglottos 👋
             </h1>
           </div>
 
